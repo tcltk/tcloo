e638a88c0805579627519eeebd899c82fd3a02d8
dkf <dkf>
2007-12-30 15:08:17 +0000

    Fix [Bug 1847313]

--- generic/tclOOStubLib.c
+++ generic/tclOOStubLib.c
@@ -1,6 +1,6 @@
 /*
- * $Id: tclOOStubLib.c,v 1.1 2007/05/18 13:19:19 dkf Exp $
- * SOURCE: tk/generic/tkStubLib.c, version 1.9 2004/03/17
+ * $Id: tclOOStubLib.c,v 1.2 2007/12/30 15:08:17 dkf Exp $
+ * ORIGINAL SOURCE: tk/generic/tkStubLib.c, version 1.9 2004/03/17
  */
 
 #include "tcl.h"
@@ -20,8 +20,8 @@ const TclOOIntStubs *tclOOIntStubsPtr;
  *	this function directly, use Tcl_OOInitStubs() macro instead.
  *
  * Results:
- *	The actual version of the package that satisfies the request, or
- *	NULL to indicate that an error occurred.
+ *	The actual version of the package that satisfies the request, or NULL
+ *	to indicate that an error occurred.
  *
  * Side effects:
  *	Sets the stub table pointer.
@@ -36,36 +36,44 @@ const char *TclOOInitializeStubs(
     const char *errMsg = NULL;
     ClientData clientData = NULL;
     const char *actualVersion =
-	    Tcl_PkgRequireEx(interp, packageName, version, exact, &clientData);
+	    Tcl_PkgRequireEx(interp, packageName,version, exact, &clientData);
     struct TclOOStubAPI *stubsAPIPtr = clientData;
-    TclOOStubs *stubsPtr = stubsAPIPtr->stubsPtr;
-    TclOOIntStubs *intStubsPtr = stubsAPIPtr->intStubsPtr;
 
-    if (!actualVersion) {
+    if (stubsAPIPtr == NULL) {
+	Tcl_ResetResult(interp);
+	Tcl_AppendResult(interp, "Error loading ", packageName, " package; ",
+		"package not present or incomplete", NULL);
+	return NULL;
+    } else {
+	TclOOStubs *stubsPtr = stubsAPIPtr->stubsPtr;
+	TclOOIntStubs *intStubsPtr = stubsAPIPtr->intStubsPtr;
+
+	if (!actualVersion) {
+	    return NULL;
+	}
+
+	if (!stubsPtr || !intStubsPtr) {
+	    errMsg = "missing stub table pointer";
+	    goto error;
+	}
+	if (stubsPtr->epoch != epoch || intStubsPtr->epoch != epoch) {
+	    errMsg = "epoch number mismatch";
+	    goto error;
+	}
+	if (stubsPtr->revision<revision || intStubsPtr->revision<revision) {
+	    errMsg = "require later revision";
+	    goto error;
+	}
+
+	tclOOStubsPtr = stubsPtr;
+	tclOOIntStubsPtr = intStubsPtr;
+	return actualVersion;
+
+    error:
+	Tcl_ResetResult(interp);
+	Tcl_AppendResult(interp, "Error loading ", packageName, " package",
+		" (requested version '", version, "', loaded version '",
+		actualVersion, "'): ", errMsg, NULL);
 	return NULL;
     }
-
-    if (!stubsPtr || !intStubsPtr) {
-	errMsg = "missing stub table pointer";
-	goto error;
-    }
-    if (stubsPtr->epoch != epoch || intStubsPtr->epoch != epoch) {
-	errMsg = "epoch number mismatch";
-	goto error;
-    }
-    if (stubsPtr->revision < revision || intStubsPtr->revision < revision) {
-	errMsg = "require later revision";
-	goto error;
-    }
-
-    tclOOStubsPtr = stubsPtr;
-    tclOOIntStubsPtr = intStubsPtr;
-    return actualVersion;
-
-  error:
-    Tcl_ResetResult(interp);
-    Tcl_AppendResult(interp, "Error loading ", packageName, " package",
-	    " (requested version '", version, "', loaded version '",
-	    actualVersion, "'): ", errMsg, NULL);
-    return NULL;
 }
