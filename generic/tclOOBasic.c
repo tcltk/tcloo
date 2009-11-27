c4666e13d17ebbcabb969dc6e1cdad50ccb6c6c7
dkf <dkf>
2009-11-27 06:38:54 +0000

    [Bug 2903811]: Remove unneeded restrictions on usefully calling the oo::object->variable method. Leaving it hidden is enough.

--- generic/tclOOBasic.c
+++ generic/tclOOBasic.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclOOBasic.c,v 1.5 2009/10/22 19:10:50 dkf Exp $
+ * RCS: @(#) $Id: tclOOBasic.c,v 1.6 2009/11/27 06:38:55 dkf Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -421,12 +421,12 @@ TclOO_Object_LinkVar(
     }
 
     /*
-     * Do nothing if we are not called from the body of a method. In this
-     * respect, we are like the [global] command.
+     * A sanity check. Shouldn't ever happen. (This is all that remains of a
+     * more complex check inherited from [global] after we have applied the
+     * fix for [Bug 2903811]; note that the fix involved *removing* code.)
      */
 
-    if (iPtr->varFramePtr == NULL ||
-	    !(iPtr->varFramePtr->isProcCallFrame & FRAME_IS_METHOD)) {
+    if (iPtr->varFramePtr == NULL) {
 	return TCL_OK;
     }
 
@@ -452,9 +452,7 @@ TclOO_Object_LinkVar(
 	 * would only work if the caller was a method of the object itself,
 	 * which might not be true if the method was exported. This is a bit
 	 * of a hack, but the simplest way to do this (pushing a stack frame
-	 * would be horribly expensive by comparison). We never have to worry
-	 * about the case where we're dealing with the global namespace; we've
-	 * already checked that we are inside a method.
+	 * would be horribly expensive by comparison).
 	 */
 
 	savedNsPtr = iPtr->varFramePtr->nsPtr;
